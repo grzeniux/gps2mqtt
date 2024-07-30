1fa25bc201407d07b3e5ffe0dbedc469b35a86d7
Grzegorz Pustelnik <grzeniux28@gmail.com>
2024-07-30 20:48:53 +0200

    Load configuration from JSON file

---
+++ gps2mqtt.py
@@ -0,0 +1,16 @@
+import os
+import json
+import paho.mqtt.client as mqtt
+import gpsd
+import pytz
+from datetime import datetime
+
+config_path = 'config.json'
+with open(config_path) as config_file:
+    config = json.load(config_file)
+
+mqtt_server = config['mqtt']['server']
+mqtt_port = config['mqtt']['port']
+mqtt_client_id = config['mqtt']['clientIdentifier']
+mqtt_topics = config['mqtt']['topics']
+
